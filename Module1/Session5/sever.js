52b3baeb18edc63b9b597ed33b2d454efa62d992
TOPICA <xero0696@gmail.com>
2019-03-13 19:54:57 +0700

    initial commit

--- Module1/Session5/sever.js
+++ Module1/Session5/sever.js
@@ -5,8 +5,14 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/views/home.html');
+app.get('/', (req,res) => {    
+    res.sendFile(__dirname + '/views/home.html')
+});
+
+app.get('/randomquestion',(req,res) => {
+    const questionList = JSON.parse(fs.readFileSync('./questions.json','utf-8'));
+    const randomQuestion = questionList[Math.floor(Math.random()*questionList.length)];
+    res.send(randomQuestion);
 });
 
 app.get('/ask', (req,res) => {
@@ -18,12 +24,25 @@ app.post('/addquestion',(req,res)=>{
     //     const stringData = data + '';
     //     console.log(stringData.split('='));
     // });
-    const question = req.body.question;
-    console.log(question);
     const questionList = JSON.parse(fs.readFileSync('./questions.json','utf-8'));
+    const question = {
+        content: req.body.question,
+        yes:0,
+        no:0,
+        id: questionList.length,
+    };
+    console.log(question);
+    
     questionList.push(question);
     fs.writeFileSync('./questions.json',JSON.stringify(questionList));
     res.redirect("/ask");
+});
+
+app.put("/editquestion", (req,res)=> {
+    const questionList = JSON.parse(fs.readFileSync('./questions.json','utf-8'));
+    const question = req.body;
+    questionList[question.id] = question;
+    fs.writeFileSync("./questions.json", JSON.stringify(questionList));
 });
 
 app.listen(3000, (err) =>{
